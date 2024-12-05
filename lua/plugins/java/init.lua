f5a5d09f60a5da89bcda0efedb70bee5dc5bf5b8
leonardo091800 <girardelloleonardo@gmail.com>
2024-12-05 21:49:26 +0100

    basic java configuration added

---
+++ lua/plugins/java/init.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {
+            -- Your custom jdtls settings goes here
+            require("lspconfig").jdtls.setup({}),
+          },
+        },
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              -- Your custom nvim-java configuration goes here
+            })
+          end,
+        },
+      },
+    },
+  },
+}
